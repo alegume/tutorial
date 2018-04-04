9bdec68da5fbd334b2ea5262c0b89dd41c6225b2
alegume <alexandreabreu@comp.ufla.br>
2018-04-03 22:19:06 -0300

    comentarios

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^post/(?P<pk>[0-9]+)/delete/$', views.post_delete, name='post_delete'),
+    url(r'^post/(?P<pk>[0-9]+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
+    url(r'^post/(?P<pk>[0-9]+)/comment_remove/$', views.comment_remove, name='comment_remove'),
+    url(r'^post/(?P<pk>[0-9]+)/comment_aprove/$', views.comment_approve, name='comment_approve'),
     url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
     url(r'^post/(?P<pk>[0-9]+)/publicar/$', views.post_publish, name='post_publish')
 ]
